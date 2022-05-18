7342b2c829128a59c54a6a3f821e5fdc1e50356f
aledezma25 <aledezma@unal.edu.co>
2022-05-18 12:25:05 -0500

    Encabezado

--- src/Componentes_generales.jsx
+++ src/Componentes_generales.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import {Encabezado} from './Encabezado';
 import {Tabla} from './Tabla';
-import {Formulario} from './Formulario';
-import {Boton} from './Boton';
-import {Pie} from './Pie';
+// import {Formulario} from './Formulario';
+// import {Boton} from './Boton';
+// import {Pie} from './Pie';
 
 export function Aplicacion(){
     return (
         <React.Fragment>
+            <Encabezado/>
             {/* <Tabla/>
             <Formulario/>
             <Tabla/> */}
-            <Encabezado/>
+            
         </React.Fragment>
     )
-}
+} 
